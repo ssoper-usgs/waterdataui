0e1d02e3e5480e1d7966f9ed3d37a65b528f7ff4
ssoper <ssoper@usgs.gov>
2019-10-18 16:38:18 -0500

    Checkpoint commit, almost returning median data for a given method id

--- assets/src/scripts/selectors/median-statistics-selector.js
+++ assets/src/scripts/selectors/median-statistics-selector.js
@@ -2,7 +2,9 @@
 import memoize from 'fast-memoize';
 import reduce from 'lodash/reduce';
 import { createSelector } from 'reselect';
-import { getCurrentParmCd } from './time-series-selector';
+import { getAllMethodsForCurrentVariable } from '../components/hydrograph/time-series';
+import {getCurrentMethodID, getCurrentParmCd } from './time-series-selector';
+
 
 /*
  * Selectors that return properties from the state
@@ -17,13 +19,58 @@ export const getMedianStatisticsByParmCd = memoize(parmCd => createSelector(
     stats => stats[parmCd] || null
 ));
 
-/*
- * @return {Object} where keys are TsID and the properties are the median data.
- */
-export const getCurrentVariableMedianStatistics = createSelector(
-    getCurrentParmCd,
-    getMedianStatistics,
-    (parmCd, stats) => stats[parmCd] || null
+// /*
+//  * @return {Object} where keys are TsID and the properties are the median data.
+//  */
+// export const getCurrentVariableMedianStatistics = createSelector(
+//     getCurrentParmCd,
+//     getMedianStatistics,
+//     (parmCd, stats) => stats[parmCd] || null
+// );
+
+ /*
+  * @return {Object} where keys are TsID and the properties are the median data.
+  */
+// Need to return the median statistics whose method description matches the current method id's method description.
+ export const getCurrentVariableMedianStatistics = createSelector(
+     getCurrentParmCd,
+     getCurrentMethodID,
+     getMedianStatistics,
+     getAllMethodsForCurrentVariable,
+         (parmCd, currentMethodId, stats, methods) => {
+             if (stats && parmCd && stats && methods) {
+                 let currentVariableMedianStatistics = stats[parmCd];
+                 if (currentVariableMedianStatistics) {
+
+                     let tsids = Object.keys(currentVariableMedianStatistics);
+
+                     let winningMethodDescription;
+
+                     for (const method of methods){
+                        if (method.methodID === currentMethodId) {
+                            console.log('the winning method id and description is: ');
+                            console.log(method.methodID);
+                            console.log(method.methodDescription);
+                            winningMethodDescription = method.methodDescription;
+                        }
+                     }
+
+                     let winningStats = {};
+                     for (const tsid of tsids) {
+                         if (currentVariableMedianStatistics[tsid].map(key => key['loc_web_ds'])[0] === winningMethodDescription) {
+                             winningStats.add(currentVariableMedianStatistics[tsid]);
+                         }
+                     }
+                     // the raw stats
+                     console.log(stats);
+                     // the stats we currently provide
+                     console.log(currentVariableMedianStatistics);
+                     // the stats we now want to provide
+                     console.log(winningStats);
+                     return winningStats || null;
+                 }
+             }
+         }
 );
 
 /*
